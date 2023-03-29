d938e02ed653447e19fdea3034b3c75cb2e9cae1
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-03-29 20:28:03 +0900

    Update 4.indexes.py

--- learning_langchain/4.indexes.py
+++ learning_langchain/4.indexes.py
@@ -1,5 +1,8 @@
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.document_loaders import UnstructuredURLLoader
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders.sitemap import SitemapLoader
 
 
@@ -8,7 +11,19 @@ documents = loader.load()
 print("=== documents[0] ===")
 print(documents[0])
 
-index = VectorstoreIndexCreator().from_loaders([loader])
-result = index.query("早野さんのことを紹介してください。")
+# ここからのコードは以下の 2 行と同じ内容です
+#
+# index = VectorstoreIndexCreator().from_loaders([loader])
+# result = index.query("StudyCoのメンバーを1人選んで紹介してください。")
+
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_documents(documents)
+embeddings = OpenAIEmbeddings()
+db = Chroma.from_documents(texts, embeddings)
+retriever = db.as_retriever()
+qa = RetrievalQA.from_chain_type(
+    llm=OpenAI(), chain_type="stuff", retriever=retriever)
+result = qa.run("StudyCoのメンバーを1人選んで紹介してください。")
+
 print("=== result ===")
 print(result)
