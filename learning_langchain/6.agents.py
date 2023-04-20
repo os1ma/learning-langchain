051bb1ad461f475840b68a7ab084e9b0be973ab0
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2023-04-20 18:20:07 +0900

    Update agents.py

--- learning_langchain/6.agents.py
+++ learning_langchain/6.agents.py
@@ -8,8 +8,8 @@ langchain.verbose = True
 
 llm = OpenAI(model_name="text-davinci-003", temperature=0)
 tools = load_tools(["terminal"], llm=llm)
-agent = initialize_agent(
+agent_executor = initialize_agent(
     tools, llm, agent="zero-shot-react-description")
 
-result = agent.run("現在のディレクトリにあるファイルの一覧を表示してください。")
+result = agent_executor.run("現在のディレクトリにあるファイルの一覧を教えてください。")
 print(result)
